84e00ce055f23053f6c2897512c2b016e26235a2
etto48 <minecraft.etto@gmail.com>
2022-10-25 12:48:05 +0200

    fixxxx2

--- Peer/video.py
+++ Peer/video.py
@@ -3,13 +3,14 @@ import threading
 import queue
 import time
 import numpy as np
+import base64
 from . import tools
 
 
 
 WAIT_FOR_QUEUE = 0.05
 
-SIZE = (640,480)
+SIZE = (50,50)
 
 video_call_peer:tools.peer = None
 
@@ -27,6 +28,7 @@ def video_call_out(target:tools.peer,stop_call_event:threading.Event):
             ret,data = video_stream.read()
             data = cv2.resize(data,SIZE)
             ret, data = cv2.imencode('.jpg', data)
+            data = base64.b64encode(data)
             send_video_buffer.put(data,block=False)
         except queue.Full:
             time.sleep(WAIT_FOR_QUEUE)
@@ -35,6 +37,8 @@ def video_call_in(target:tools.peer,stop_call_event:threading.Event):
     while not stop_call_event.is_set():
         try:
             data = recv_video_buffer.get(block=False)
+            data = base64.b64decode(data)
+            data = np.frombuffer(data)
             data = cv2.imdecode(data, 1)
             cv2.imshow(f"{video_call_peer.nickname}",data)
             cv2.waitKey(1)
@@ -42,10 +46,15 @@ def video_call_in(target:tools.peer,stop_call_event:threading.Event):
             time.sleep(WAIT_FOR_QUEUE)
     cv2.destroyAllWindows()
 
+video_call_input_thread:threading.Thread = None
+video_call_output_thread:threading.Thread = None
+
 def start_video_call(target:tools.peer):
     global video_call_peer
     global video_stream
     global stop_call
+    global video_call_input_thread
+    global video_call_output_thread
 
     if video_call_peer is not None:
         return
